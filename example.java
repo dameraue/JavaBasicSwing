2731fd0c320e9104194191ef1d46cbab3c985741
dameraue <50361197+dameraue@users.noreply.github.com>
2021-11-21 12:05:25 +0100

    Update example.java
    update commit mit kommentaren

--- example.java
+++ example.java
@@ -1,16 +1,63 @@
 import javax.swing.*;
-class gui{
+/* 
+import der javax swing library 
+Aufruf Klasse newWindow
+*/
+class newWindow{
+
+/*
+main Methode 
+*/
     public static void main(String args[]){
-       JFrame frame = new JFrame("My First GUI");
+
+    //public String displayVorname = public String vorname;
+       JFrame frame = new JFrame("Einfach Java mit Swing");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       frame.setSize(700,500);
-       JButton button = new JButton("Press");
-       frame.getContentPane().add(button); // Adds Button to content pane of frame
+       frame.setSize(300,300);
+       //JButton button = new JButton("Press");
+     //  frame.getContentPane().add(button); 
+/*
+Einfaches Label auf Window. Kann auch genutzt werden, um Klasse Kunde auszugeben
+*/
+    JLabel vornameLabel = new JLabel("Position eines einfachen Labels, kann auch Variablen ausgeben.");
+     frame.getContentPane().add(vornameLabel);
        frame.setVisible(true);
+       System.out.print(vornameLabel.getText());
+       
     }
 }
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-} 
+/*
+Klasse Kunde wie in Kapitel 4/4
+*/
+class Kunde {   
+    public String name;
+    public String vorname;
+    public String geschlecht;
+    public String geburtsdatum;     
+        public String getName() {
+            name= "Skywalker";
+            return name;
+        }
+        public String getVorname() {
+            vorname = "Luke";
+            return vorname;
+        }
+        public String getGeschlecht() {
+            geschlecht = "w";
+            return geschlecht;
+        }
+        public String getGeburtsdatum() {
+            geburtsdatum = "01.01.2001";
+            return geburtsdatum;
+        }
+      /* 
+Mit public void setName kann der oben zugewisene name neu deklariert werden
+
+       public void setName(String name) {
+            this.name = name;
+        }
+        */
+        }  
+
+   
+
